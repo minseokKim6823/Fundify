b0ceabd0778e394e9d57907acb4bfbe8f7b5732e
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-18 17:50:17 +0900

    ✨BUILD : SpringBoot 3.2.5 기반 서버 초기 세팅 #1

---
+++ src/main/java/com/fundify/FundifyApplication.java
@@ -0,0 +1,13 @@
+package com.fundify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fundify/FundifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
